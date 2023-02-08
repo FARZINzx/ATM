8722fbaff3c605e688cf8eaa7cc9b1b92ca8f321
Farzin Hamzehi <5farzinhamzei@gmail.com>
2023-02-09 02:35:50 +0330

    Define functions to project

--- main.c
+++ main.c
@@ -4,3 +4,102 @@
 #include <ctype.h>
 #include <windows.h>
 #include <conio.h>
+//
+//Define functions
+//main menu
+void mainMenu();
+// function for show employee item
+void employeeMenu();
+// function for show customer item
+void customerMenu();
+// function for exit as app
+void exitMenu();
+//function for create an account in file
+void createAccount();
+//function for edit data as file
+void editAccount();
+// function for check login edit
+int editLoginCheck();
+//function for show the richest customer
+void showRichestCustomer();
+//function for show and sum all of account
+void showTotalAccount();
+//function for back main menu
+void backMainMenu();
+//function for show customer data
+void accountBalance();
+//function for transfer money from one account to another account
+void cashTransfer();
+//function for withdraw money from account
+void cashWithDrawl();
+//function for show 10 recent account update
+void recentAccountUpdates();
+//function for change password of account
+void changePassword();
+//function for check login employee
+int loginEmployee();
+//function for check login customer
+int loginCustomer();
+//function for show data of customer
+void showAccount();
+//function for save file in data
+void saveToFile();
+//function for read file from file
+int readFromFile();
+// function for save index of customer from file
+void saveIndexOfAccount();
+// function for read index of customer from file
+void readIndexOfAccount();
+// function for back to customer menu in app
+void backToCustomerMenu();
+// function for back to employee menu in app
+void backToEmployeeMenu();
+
+
+void mainMenu(){};
+// function for show employee item
+void employeeMenu(){};
+// function for show customer item
+void customerMenu(){};
+// function for exit as app
+void exitMenu(){};
+//function for create an account in file
+void createAccount(){};
+//function for edit data as file
+void editAccount(){};
+// function for check login edit
+int editLoginCheck(){};
+//function for show the richest customer
+void showRichestCustomer(){};
+//function for show and sum all of account
+void showTotalAccount(){};
+//function for back main menu
+void backMainMenu(){};
+//function for show customer data
+void accountBalance(){};
+//function for transfer money from one account to another account
+void cashTransfer(){};
+//function for withdraw money from account
+void cashWithDrawl(){};
+//function for show 10 recent account update
+void recentAccountUpdates(){};
+//function for change password of account
+void changePassword(){};
+//function for check login employee
+int loginEmployee(){};
+//function for check login customer
+int loginCustomer(){};
+//function for show data of customer
+void showAccount(){};
+//function for save file in data
+void saveToFile(){};
+//function for read file from file
+int readFromFile(){};
+// function for save index of customer from file
+void saveIndexOfAccount(){};
+// function for read index of customer from file
+void readIndexOfAccount(){};
+// function for back to customer menu in app
+void backToCustomerMenu(){};
+// function for back to employee menu in app
+void backToEmployeeMenu(){};
