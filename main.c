c76b02ac04b24d114f3af56cef6209f29b09ed7f
Farzin Hamzehi <5farzinhamzei@gmail.com>
2023-02-09 03:15:18 +0330

    create employee login and main menu

--- main.c
+++ main.c
@@ -55,10 +55,46 @@ void backToCustomerMenu();
 // function for back to employee menu in app
 void backToEmployeeMenu();
 
+int main(){
+    mainMenu();
+    return 0;
+}
 
-void mainMenu(){};
+
+void mainMenu(){
+    // Define Character
+    int choice;
+    // Item menu
+    printf("1) Log in as an employee\n");
+    printf("2) Log in as a customer\n");
+    printf("3) Exit\n");
+    while (1) {
+        scanf("%d", &choice);
+        if (choice >= 1 && choice < 4) {
+            switch (choice) {
+                case 1:
+                    employeeMenu();
+                    break;
+                case 2:
+                    customerMenu();
+                    break;
+                case 3:
+                    exitMenu();
+                    break;
+            }
+        } else {
+            printf("Please select the one item!\n");
+            continue;
+        }
+    }
+};
 // function for show employee item
-void employeeMenu(){};
+void employeeMenu(){
+    system("cls");
+    if(loginEmployee()==1){
+        printf("dfe");
+    }
+}
 // function for show customer item
 void customerMenu(){};
 // function for exit as app
@@ -86,7 +122,41 @@ void recentAccountUpdates(){};
 //function for change password of account
 void changePassword(){};
 //function for check login employee
-int loginEmployee(){};
+int loginEmployee(){
+    // Define Variable
+    int value, index = 0;
+    int Max = 10;
+    int passWord;
+    char userName[Max];
+    int pass = 1234;
+    char user[6] = "admin";
+    system("cls");
+    do {
+        //
+            if (index > 0) {
+                system("cls");
+                printf("The password or username is valid!\n");
+            }
+            printf("*** Login ***\n");
+            //
+            printf("Username :\n");
+            scanf("%s", userName);
+            getchar();
+            printf("Password :\n");
+            scanf("%d", &passWord);
+            if (strcmp(userName, user) == 0 && passWord == pass) {
+                value = 1;
+                return 1;
+            } else {
+                value = 0;
+            }
+            if ((passWord != pass) || (strcmpi(userName, user) != 0)) {
+                printf("The password or username is valid!\n");
+                index++;
+                continue;
+            }
+    } while (value == 0);
+};
 //function for check login customer
 int loginCustomer(){};
 //function for show data of customer
